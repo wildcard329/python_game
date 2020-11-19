4aa0c6b088e85d3071fd8316c429ecbfe92434a4
wildcard329 <enkiduwildman@gmail.com>
2020-11-18 23:29:00 -0800

    worked on parser

--- parser.py
+++ parser.py
@@ -1,4 +1,9 @@
 class Parser:
     def __init__(self, command):
         self.command = command
+        self.action = None
+        self.argument = None
         
+    def parse_command(self):
+        self.action = self.command[0]
+        self.argument = self.command[:1]
